2df3d806acfda399ba318bb418b59085ccb744e4
Tony H Dillion <lake1355@qq.com>
2023-06-24 21:35:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const { withSentryConfig } = require('@sentry/nextjs')
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
